187a7443e5b8c4e3ac4f7f8f7fca8893b59a47ff
Logan Zartman <logan.zartman@utexas.edu>
2018-01-06 10:50:23 -0600

    Re-add dat.gui to demo

--- demo/App.vue
+++ demo/App.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+const dat = require("../src/dat.gui.min.js");
+const chroma = require("../src/chroma.min.js");
 import Planet from "../src/Planet.js";
 import PlanetDisplay from "../src/PlanetDisplay";
 
@@ -18,7 +20,7 @@ export default {
     }
   },
   created() {
-    this.planet = new Planet({
+    const planetData = {
         "seed": 0x42069,
         "size": 0.7,
         "water": 0.5,
@@ -27,7 +29,37 @@ export default {
         "baseColor": 0xa4be92,
         "accColor": 0xf5dac3,
         "numTerrains": 4
-    });
+    };
+    this.planet = new Planet(planetData);
+    this.createGui(planetData);
+  },
+  methods: {
+    createGui(traits) {
+      let gui = new dat.gui.GUI();
+      const update = () => this.planet.rebuild();
+
+      gui.remember(traits);
+      gui.add(traits, "seed", 0, 0xFFFFFFFF, 1).onChange(update).listen();
+      gui.add(traits, "size", 0, 1).onChange(update).listen();
+      gui.add(traits, "water", 0, 1).onChange(update).listen();
+      gui.add(traits, "atmoDensity", 0, 1).onChange(update).listen();
+      gui.add(traits, "cloudDensity", 0, 1).onChange(update).listen();
+      gui.addColor(traits, "baseColor").onChange(update).listen();
+      gui.addColor(traits, "accColor").onChange(update).listen();
+      gui.add(traits, "numTerrains", 2, 8, 1).onChange(update).listen();
+
+      gui.add({"random": () => {
+        traits.seed = Math.floor(Math.random() * 0xFFFFFFFF);
+        traits.size = Math.random() * 0.7 + 0.3;
+        traits.water = Math.random();
+        traits.atmoDensity = Math.random();
+        traits.cloudDensity = Math.random();
+        traits.baseColor = chroma.random().desaturate(1).css();
+        traits.accColor = chroma.random().desaturate(1).css();
+        traits.numTerrains = Math.round(Math.random()*6)+2;
+        update();
+      }}, "random");
+    }
   },
   components: {PlanetDisplay}
 }
